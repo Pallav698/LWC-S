74bccc343133c328518346c9373e2be9cbac984a
Pallav <iampallav698@gmail.com>
2024-12-25 14:31:25 +0530

    Search Component with debouncing

--- force-app/main/default/lwc/dynamicSeacrh/dynamicSeacrh.js
+++ force-app/main/default/lwc/dynamicSeacrh/dynamicSeacrh.js
@@ -2,30 +2,39 @@ import { LightningElement } from 'lwc';
 import getContactsAndAccount from '@salesforce/apex/ContactsHandler.getContactsAndAccount';
 
 export default class DynamicSeacrh extends LightningElement {
+    searchTimeout;
+    isLoading = false;
+
     handleSearch(event){
         const searchTerm = event.target.value.trim();
 
-        if(searchTerm !== ''){
-            this.fetchContact(searchTerm);
+        if (this.searchTimeout) {
+            clearTimeout(this.searchTimeout);
         }
-        else{
+
+        if(searchTerm !== ''){  
+            this.isLoading = true;
+            this.searchTimeout = setTimeout(() => {
+                this.fetchContact(searchTerm);
+            }, 300);
+        } else {
             this.contacts = [];
         }
-        
     }
 
     contacts = [];
 
     fetchContact(searchTerm){
-        getContactsAndAccount({ searchTerm: searchTerm})
-        .then(contacts => {
-            this.contacts = contacts;
-            console.log('this.contacts ',this.contacts);
-        })
-        .catch(error => {
-            console.error('Error fetching contacts:', error);
-        });
+        getContactsAndAccount({ searchTerm: searchTerm })
+            .then(contacts => {
+                this.contacts = contacts;
+                console.log('this.contacts ', this.contacts);
+            })
+            .catch(error => {
+                console.error('Error fetching contacts:', error);
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
     }
-
-
 }
